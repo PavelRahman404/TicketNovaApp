11718bfa55788788f2ed3f3373692ee79120695f
PavelRahman404 <paveltauker@gmail.com>
2024-10-02 00:18:19 +0600

    First Migration

---
+++ TicketNova/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNova_Domain.Entities;
+
+namespace TicketNova.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
